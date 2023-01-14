28b9ed941d83e76665ed0cc553c8d3cf5cd04cf6
Chris Churas <churas.camera@gmail.com>
2023-01-13 16:43:16 -0800

    started updating

--- cdhierarchyfromcdaps/cdhierarchyfromcdapscmd.py
+++ cdhierarchyfromcdaps/cdhierarchyfromcdapscmd.py
@@ -6,6 +6,7 @@ import argparse
 import traceback
 import json
 import ndex2
+import cdapsutil
 
 
 class Formatter(argparse.ArgumentDefaultsHelpFormatter,
@@ -23,21 +24,8 @@ def _parse_arguments(desc, args):
     parser = argparse.ArgumentParser(description=desc,
                                      formatter_class=Formatter)
     parser.add_argument('input',
-                        help='Network in CX format')
-    parser.add_argument('--n', type=int,
-                        help='Target community number. Explore the'
-                             'maximum resolution parameter until the '
-                             'number of generated communities at this '
-                             'resolution is close enough to this value. '
-                             'Increase to get more smaller communities')
-    parser.add_argument('--weight',
-                        help='Optional, name of node column containing edge weights')
-    parser.add_argument('--default', default=0.0,
-                        help='if --weight is set, but a given edge lacks a value, this'
-                             'value will be output instead')
-    parser.add_argument('--failonmissingweight', action='store_true',
-                        help='If set, fail if weight column is set by and one or '
-                             'more edges lack a weight value')
+                        help='JSON')
+
     return parser.parse_args(args)
 
 
@@ -70,36 +58,20 @@ def run_cxtoedgelist(theargs, out_stream=sys.stdout,
         err_stream.write(str(theargs.input) + ' is an empty file')
         return 4
     try:
-        weight_col = None
-        default_weight = str(theargs.default)
+
         with open(theargs.input, 'r') as f:
             raw_json = json.load(f)
-            if 'weight' in raw_json:
-                weight_col = raw_json['weight']
-                if weight_col is not None and len(str(weight_col).strip()) == 0:
-                    weight_col = None
+            if 'communitydetectresultv2' not in raw_json:
+                return 5
             if 'cx' not in raw_json:
-                # just try to read the CX directly
-                raw_json_to_load = raw_json
-            else:
-                raw_json_to_load = raw_json['cx']
+                return 6
+
+            raw_json_to_load = raw_json['cx']
             net = ndex2.create_nice_cx_from_raw_cx(raw_json_to_load)
 
-            for edge_id, edge_obj in net.get_edges():
-                out_stream.write(str(edge_obj['s']) + '\t' + str(edge_obj['t']))
-                if weight_col is not None:
-                    edge_attr = net.get_edge_attribute(edge_id, weight_col)
-                    if edge_attr == (None, None) or edge_attr is None:
-                        if theargs.failonmissingweight:
-                            err_stream.write('\nEdge: ' + str(edge_obj) +
-                                             ' lacks a value for '
-                                             'weight column: ' +
-                                             str(weight_col) + '\n')
-                            return 5
-                        out_stream.write('\t' + default_weight)
-                    else:
-                        out_stream.write('\t' + str(edge_attr['v']))
-                out_stream.write('\n')
+            cd = cdapsutil.CommunityDetection(runner=cdapsutil.ExternalResultsRunner())
+            cd.
+
         return 0
     finally:
         err_stream.flush()
@@ -114,9 +86,8 @@ def main(args):
     :rtype: int
     """
     desc = """
-    Takes either a CX file or a CXWITHWEIGHT json file and writes out
-    an edge list in EDGELIST format that can be fed to CDAPS tools
-    
+    Takes COMMUNITYDETECTRESULTV2 with CX and outputs a CDAPS compatible
+    hierarchy
     
     """
     theargs = _parse_arguments(desc, args[1:])
